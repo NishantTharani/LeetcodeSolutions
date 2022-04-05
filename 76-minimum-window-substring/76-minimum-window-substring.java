0fb68591916524f0edbee315269aeb8c32736f8e
NishantTharani <33641044+NishantTharani@users.noreply.github.com>
2022-04-04 18:34:46 -0700

    Time: 28 ms (33.42%), Space: 51.1 MB (23.85%) - LeetHub

--- 76-minimum-window-substring/76-minimum-window-substring.java
+++ 76-minimum-window-substring/76-minimum-window-substring.java
@@ -2,50 +2,48 @@ class Solution {
     public String minWindow(String s, String t) {
         String out = "";
         Integer len = Integer.MAX_VALUE;
+        int matches = 0;
+        int start = 0;
         
         if (t.length() > s.length())
             return out;
-        
+
         // Build frequency map of t
-        Map<Character, Integer> tmap = new HashMap<>();
+        Map<Character, Integer> map = new HashMap<>();
         
         for (char c : t.toCharArray()) {
-            tmap.put(c, tmap.getOrDefault(c, 0) + 1);
+            map.put(c, map.getOrDefault(c, 0) + 1);
         }
-        
-        Map<Character, Integer> map = new HashMap<>();
-        int start = 0;
         
         for (int end = 0; end < s.length(); end++) {
             char endChar = s.charAt(end);
-            map.put(endChar, map.getOrDefault(endChar, 0) + 1);
             
-            while (hasAllChars(map, tmap)) {
-                if (end - start + 1 < len) {
-                    len = end - start + 1;
-                    out = s.substring(start, end+1);
+            if (map.containsKey(endChar)) {
+                map.put(endChar, map.get(endChar) - 1);
+                if (map.get(endChar) == 0) {
+                    matches++;
+                }
+            }
+            
+            while (matches == map.size()) {
+                if (end + 1 - start < len) {
+                    len = end + 1 - start;
+                    out = s.substring(start, end + 1);
                 }
                 
                 char startChar = s.charAt(start);
-                map.put(startChar, map.get(startChar) - 1);
+                if (map.containsKey(startChar)) {
+                    if (map.get(startChar) == 0) {
+                        matches--;
+                    }
+                    
+                    map.put(startChar, map.get(startChar) + 1);
+                }
+                
                 start++;
             }
         }
-        
         return out;
-    }
-    
-    private boolean hasAllChars(Map<Character, Integer> container, 
-                                Map<Character, Integer> target) {
-        
-        
-        for (Map.Entry<Character, Integer> entry : target.entrySet()) {
-            if (!container.containsKey(entry.getKey()) || 
-               container.get(entry.getKey()) < entry.getValue())
-                return false;
-        }
-        
-        return true;
     }
     
 }
