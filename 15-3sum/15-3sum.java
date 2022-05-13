f9d737a211220692242be3b3eece12321219e5ba
NishantTharani <33641044+NishantTharani@users.noreply.github.com>
2022-05-13 12:16:39 -0700

    Time: 33 ms (51.01%), Space: 60.1 MB (26.99%) - LeetHub

--- 15-3sum/15-3sum.java
+++ 15-3sum/15-3sum.java
@@ -1,25 +1,53 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        Set<List<Integer>> outSet = new HashSet<List<Integer>>();
+        List<List<Integer>> out = new ArrayList<List<Integer>>();
         
-        for (int i = 0; i < nums.length; i++) {
-            int target = nums[i];
-            Map<Integer, Integer> map = new HashMap<>();
+        if (nums.length < 3)
+            return out;
+        
+        // Sort it first  
+        Arrays.sort(nums);
+        
+        int x, y, z;
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i > 0 && nums[i] == nums[i-1])
+                continue;
             
-            for (int j = i+1; j < nums.length; j++) {
-                if (map.containsKey(nums[j])) {
-                    List<Integer> tmp = new ArrayList<>();
-                    tmp.add(nums[i]);
-                    tmp.add(nums[map.get(nums[j])]);
-                    tmp.add(nums[j]);
-                    Collections.sort(tmp);
-                    outSet.add(tmp);
+            x = nums[i];
+            
+            int start = i+1;
+            int end = nums.length - 1;
+            
+            while (start < end) {
+                y = nums[start];
+                z = nums[end];
+                if (start > i+1 && y == nums[start-1]) {
+                    start++;
+                    continue;
                 }
-                map.put(0 - target - nums[j], j);
+                
+                if (end < nums.length - 1 && z == nums[end+1]) {
+                    end--;
+                    continue;
+                }
+                
+                if (x + y + z == 0) {
+                    List<Integer> tmp = new ArrayList<>();
+                    tmp.add(x);
+                    tmp.add(y);
+                    tmp.add(z);
+                    out.add(tmp);
+                    start++;
+                } else if (x + y + z < 0) {
+                    start++;
+                } else {
+                    end--;
+                }
             }
+            
         }
         
-        List<List<Integer>> out = new ArrayList<>(outSet);
         return out;
+        
     }
 }
