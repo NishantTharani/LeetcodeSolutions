a968d43691808937871ef1c26177e8e41c4bc012
NishantTharani <33641044+NishantTharani@users.noreply.github.com>
2022-05-22 13:54:13 -0700

    Time: 4 ms (19.38%), Space: 47.8 MB (36.00%) - LeetHub

--- 57-insert-interval/57-insert-interval.java
+++ 57-insert-interval/57-insert-interval.java
@@ -1,32 +1,26 @@
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
         List<int[]> list = new ArrayList<>(Arrays.asList(intervals));
-        int n = intervals.length;
-        int i = 0;
+        int idx = 0;
         
-        while (i < n && newInterval[0] > list.get(i)[0]) {
-            i++;
+        while (idx < intervals.length && newInterval[0] > list.get(idx)[0]) {
+            idx++;
         }
-        
-        // If we add newInterval at index i now, list will be sorted by the interval starts, in non-descending order
-        list.add(i, newInterval);
-        
+    
+        list.add(idx, newInterval);
+            
         // Now repeat the merge intervals process for the entire list
         List<int[]> out = new ArrayList<>();
-        n = list.size();
         int start = list.get(0)[0];
         int end = list.get(0)[1];
-        i = 1;
         
-        while (i < n) {
+        for (int i = 1; i < list.size(); i++) {
             if (end < list.get(i)[0]) {
                 out.add(new int[]{start, end});
                 start = list.get(i)[0];
                 end = list.get(i)[1];
-                i++;
             } else {
                 end = Math.max(end, list.get(i)[1]);
-                i++;
             }
         }
         
