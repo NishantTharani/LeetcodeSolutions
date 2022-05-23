7c6fc558707a1bf2860163f4ffbfed2960d08832
NishantTharani <33641044+NishantTharani@users.noreply.github.com>
2022-05-23 15:01:41 -0700

    Time: 17 ms (12.06%), Space: 46 MB (13.80%) - LeetHub

--- 253-meeting-rooms-ii/253-meeting-rooms-ii.java
+++ 253-meeting-rooms-ii/253-meeting-rooms-ii.java
@@ -2,7 +2,7 @@ class Solution {
     public int minMeetingRooms(int[][] intervals) {
         Arrays.sort(intervals, Comparator.comparingInt(o -> o[0]));
         
-        List<Integer> ends = new ArrayList<>();
+        PriorityQueue<Integer> ends = new PriorityQueue<>();
         ends.add(intervals[0][1]);
         
         int start;
@@ -11,21 +11,15 @@ class Solution {
         for (int i = 1; i < intervals.length; i++) {
             start = intervals[i][0];
             end = intervals[i][1];
-            added = false;
             
-            for (int room = 0; room < ends.size(); room++) {
-                if (start >= ends.get(room)) {
-                    // This meeting can be scheduled in room
-                    ends.set(room, end);
-                    added = true;
-                    break;
-                }
-            }
-            
-            if (!added) {
-                // Meeting did not fit in any room: need a new room
+            if (start >= ends.peek()) {
+                // Replace the min value with end
+                ends.poll();
                 ends.add(end);
-            }   
+            } else {
+                // Meeting needs a new room
+                ends.add(end);
+            }
         }
         
         return ends.size();
