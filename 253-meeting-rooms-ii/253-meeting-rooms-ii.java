e5a9a50baca995992a53e7af9076031f0908f95f
NishantTharani <33641044+NishantTharani@users.noreply.github.com>
2022-05-23 14:57:13 -0700

    Time: 20 ms (7.70%), Space: 45.5 MB (34.34%) - LeetHub

--- 253-meeting-rooms-ii/253-meeting-rooms-ii.java
+++ 253-meeting-rooms-ii/253-meeting-rooms-ii.java
@@ -2,28 +2,21 @@ class Solution {
     public int minMeetingRooms(int[][] intervals) {
         Arrays.sort(intervals, Comparator.comparingInt(o -> o[0]));
         
-        List<List<int[]>> rooms = new ArrayList<>();
-        rooms.add(new ArrayList<>());
-        rooms.get(0).add(new int[]{intervals[0][0], intervals[0][1]});
-        
-        List<Integer> pos = new ArrayList<>();
-        pos.add(0);
+        List<Integer> ends = new ArrayList<>();
+        ends.add(intervals[0][1]);
         
         int start;
         int end;
-        int p;
         boolean added;
         for (int i = 1; i < intervals.length; i++) {
             start = intervals[i][0];
             end = intervals[i][1];
             added = false;
             
-            for (int room = 0; room < pos.size(); room++) {
-                p = pos.get(room);
-                if (intervals[i][0] >= rooms.get(room).get(p)[1]) {
+            for (int room = 0; room < ends.size(); room++) {
+                if (start >= ends.get(room)) {
                     // This meeting can be scheduled in room
-                    rooms.get(room).add(new int[]{start, end});
-                    pos.set(room, p + 1);
+                    ends.set(room, end);
                     added = true;
                     break;
                 }
@@ -31,12 +24,10 @@ class Solution {
             
             if (!added) {
                 // Meeting did not fit in any room: need a new room
-                rooms.add(new ArrayList<>());
-                rooms.get(rooms.size() - 1).add(new int[]{start, end});
-                pos.add(0);
+                ends.add(end);
             }   
         }
         
-        return pos.size();
+        return ends.size();
     }
 }
