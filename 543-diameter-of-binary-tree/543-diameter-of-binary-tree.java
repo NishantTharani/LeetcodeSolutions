aa8d9ead49ee8b6ef8452e9d11758c7288957e73
NishantTharani <33641044+NishantTharani@users.noreply.github.com>
2022-06-07 14:56:45 -0700

    Time: 0 ms (100.00%), Space: 42.1 MB (84.25%) - LeetHub

--- 543-diameter-of-binary-tree/543-diameter-of-binary-tree.java
+++ 543-diameter-of-binary-tree/543-diameter-of-binary-tree.java
@@ -22,35 +22,26 @@ class Solution {
         
         this.out = 0;
         
-        recDiameter(root, new HashMap<TreeNode, Integer>(), new ArrayList<TreeNode>());
+        recDiameter(root);
         
         return this.out;
     }
     
-    private int recDiameter(TreeNode curr, Map<TreeNode, Integer> map, List<TreeNode> path) {
+    private int recDiameter(TreeNode curr) {
         if (curr.left == null && curr.right == null) {
-            for (int i = 0; i < path.size(); i++) {
-                this.out = Math.max(this.out, path.size() - i + map.getOrDefault(path.get(i), 0));
-            }
-            
             return 1;
         }
         
         int left = 0;
         int right = 0;
-        path.add(curr);
         
         if (curr.left != null)
-            left = recDiameter(curr.left, map, path);
-        
-        map.put(curr, Math.max(map.getOrDefault(curr, 0), left));
+            left = recDiameter(curr.left);
         
         if (curr.right != null)
-            right = recDiameter(curr.right, map, path);
+            right = recDiameter(curr.right);
         
-        map.put(curr, Math.max(map.getOrDefault(curr, 0), right));
-        
-        path.remove(path.size() - 1);
+        this.out = Math.max(this.out, left + right);
         
         return Math.max(left, right) + 1;
     }
