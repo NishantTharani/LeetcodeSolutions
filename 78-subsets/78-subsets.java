1d215f233a49aba06ece5530bc095b4a23e3de2c
NishantTharani <33641044+NishantTharani@users.noreply.github.com>
2022-06-09 16:03:22 -0700

    Time: 1 ms (81.01%), Space: 43.8 MB (11.02%) - LeetHub

--- 78-subsets/78-subsets.java
+++ 78-subsets/78-subsets.java
@@ -1,32 +1,18 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
-        Queue<List<Integer>> queue = new LinkedList<>();
-        List<List<Integer>> out = new ArrayList<>();
-        out.add(new ArrayList<Integer>());
+        List<List<Integer>> subsets = new ArrayList<>();
+        subsets.add(new ArrayList<Integer>());
         
-        for (int i = 0; i < nums.length; i++) {
-            List<Integer> tmp = new ArrayList<>();
-            tmp.add(i);
-            queue.add(tmp);
-        }
-        
-        while (queue.size() > 0) {
-            List<Integer> idxs = queue.poll();            
-            List<Integer> vals = new ArrayList<>();
-            
-            for (int idx : idxs) {
-                vals.add(nums[idx]);
-            }
-            out.add(vals);
-            
-            int end = idxs.get(idxs.size() - 1);
-            for (int j = end + 1; j < nums.length; j++) {
-                List<Integer> newIdxs = new ArrayList<>(idxs);
-                newIdxs.add(j);
-                queue.add(newIdxs);
+        for (int num : nums) {
+            int n = subsets.size();
+            for (int i = 0; i < n; i++) {
+                List<Integer> base = subsets.get(i);
+                List<Integer> next = new ArrayList<Integer>(base);
+                next.add(num);
+                subsets.add(next);
             }
         }
         
-        return out;
+        return subsets;
     }
 }
