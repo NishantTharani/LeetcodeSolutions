15d33bd6a93bc8d3fc5494e80ca5092b8c194708
NishantTharani <33641044+NishantTharani@users.noreply.github.com>
2022-06-12 16:00:49 -0700

    Time: 11 ms (33.57%), Space: 54.3 MB (35.60%) - LeetHub

--- 784-letter-case-permutation/784-letter-case-permutation.java
+++ 784-letter-case-permutation/784-letter-case-permutation.java
@@ -1,37 +1,37 @@
 class Solution {
     public List<String> letterCasePermutation(String s) {
         List<String> out = new ArrayList<>();
-        Queue<String> perms = new LinkedList<>();
-        perms.add("");
+        Queue<StringBuilder> perms = new LinkedList<>();
+        perms.add(new StringBuilder(""));
         
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
             int n = perms.size();
             
             for (int j = 0; j < n; j++) {
-                String base = perms.poll();
+                StringBuilder base = perms.poll();
                 
                 if (Character.isDigit(c)) {
-                    String next = base + c;
-                    if (next.length() == s.length()) {
-                        out.add(next);
+                    base.append(c);
+                    if (base.length() == s.length()) {
+                        out.add(base.toString());
                     } else {
-                        perms.add(next);
+                        perms.add(base);
                     }
                 } else {
-                    String next1 = base + c;
-                    String next2;
+                    StringBuilder copy = new StringBuilder(base.toString());
+                    base.append(c);
                     if (Character.isLowerCase(c)) {
-                        next2 = base + Character.toUpperCase(c);
+                        copy.append(Character.toUpperCase(c));
                     } else {
-                        next2 = base + Character.toLowerCase(c);
+                        copy.append(Character.toLowerCase(c));
                     }
-                    if (next1.length() == s.length()) {
-                        out.add(next1);
-                        out.add(next2);
+                    if (base.length() == s.length()) {
+                        out.add(base.toString());
+                        out.add(copy.toString());
                     } else {
-                        perms.add(next1);
-                        perms.add(next2);
+                        perms.add(base);
+                        perms.add(copy);
                     }
                 }
             }
