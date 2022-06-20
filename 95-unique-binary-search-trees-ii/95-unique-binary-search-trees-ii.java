e0ef0e1d84c327d550090e0a34f29fda797f8736
NishantTharani <33641044+NishantTharani@users.noreply.github.com>
2022-06-19 18:47:38 -0700

    Time: 2 ms (76.18%), Space: 45.6 MB (69.02%) - LeetHub

--- 95-unique-binary-search-trees-ii/95-unique-binary-search-trees-ii.java
+++ 95-unique-binary-search-trees-ii/95-unique-binary-search-trees-ii.java
@@ -15,22 +15,39 @@
  */
 class Solution {
     public List<TreeNode> generateTrees(int n) {
-        return recGenerate(1, n);
+        Map<Pair<Integer, Integer>, List<TreeNode>> memory = new HashMap<>();
+        return recGenerate(1, n, memory);
     }
     
-    public List<TreeNode> recGenerate(int left, int right) {
+    public List<TreeNode> recGenerate(int left, int right, Map<Pair<Integer, Integer>, List<TreeNode>> memory) {
         
         if (right - left < 0) {
             List<TreeNode> nullOut = new ArrayList<TreeNode>();
             nullOut.add(null);
+            memory.put(new Pair<Integer, Integer>(left, right), nullOut);
             return nullOut;
         }
         
         List<TreeNode> out = new ArrayList<>();
         
         for (int i = left; i <= right; i++) {
-            List<TreeNode> leftTrees = recGenerate(left, i-1);
-            List<TreeNode> rightTrees = recGenerate(i+1, right);
+            Pair<Integer, Integer> leftPair = new Pair<>(left, i-1);
+            List<TreeNode> leftTrees;
+            if (memory.containsKey(leftPair)) {
+                leftTrees = memory.get(leftPair);
+            } else {
+                leftTrees = recGenerate(left, i-1, memory);
+                memory.put(leftPair, leftTrees);
+            }
+            
+            Pair<Integer, Integer> rightPair = new Pair(i+1, right);
+            List<TreeNode> rightTrees;
+            if (memory.containsKey(rightPair)) {
+                rightTrees = memory.get(rightPair);
+            } else {
+                rightTrees = recGenerate(i+1, right, memory);
+                memory.put(rightPair, rightTrees);
+            }
             
             for (TreeNode leftTree : leftTrees) {
                 for (TreeNode rightTree : rightTrees) {
