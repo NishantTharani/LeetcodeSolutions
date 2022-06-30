25c857e5a90a6454d44dcaeff9809fcb077be931
NishantTharani <33641044+NishantTharani@users.noreply.github.com>
2022-06-29 17:02:45 -0700

    Time: 4 ms (69.22%), Space: 40.4 MB (91.42%) - LeetHub

---
+++ 767-reorganize-string/767-reorganize-string.java
@@ -0,0 +1,41 @@
+class Solution {
+    public String reorganizeString(String s) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        
+        for (int i = 0; i < s.length(); i++) {
+            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
+            if (map.get(s.charAt(i)) > (s.length() + 1)/2) {
+                return "";
+            }
+        }
+        
+        PriorityQueue<Map.Entry<Character, Integer>> minHeap = new PriorityQueue<>((e1, e2) -> e1.getValue() - e2.getValue());
+        PriorityQueue<Map.Entry<Character, Integer>> maxHeap = new PriorityQueue<>((e1, e2) -> e2.getValue() - e1.getValue());
+        
+        for (Map.Entry<Character, Integer> e : map.entrySet()) {
+            minHeap.add(e);
+            maxHeap.add(e);
+        }
+        
+        char[] out = new char[s.length()];
+        int idx = 0;
+        Map.Entry<Character, Integer> curr;
+        
+        while (!maxHeap.isEmpty()) {
+            curr = maxHeap.poll();
+            for (int i = 0; i < curr.getValue(); i++) {
+                out[idx] = curr.getKey();
+                idx += 2;
+                
+                if (idx >= s.length()) {
+                    idx = 1;
+                }
+            }
+        }
+        
+        return new String(out);
+        
+    }
+}
+
+
