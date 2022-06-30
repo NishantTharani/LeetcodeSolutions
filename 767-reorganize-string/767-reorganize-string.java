b67e81f30990ea0544800e46931e7cbef767de0f
NishantTharani <33641044+NishantTharani@users.noreply.github.com>
2022-06-29 17:08:47 -0700

    Time: 8 ms (33.62%), Space: 42.4 MB (43.11%) - LeetHub

--- 767-reorganize-string/767-reorganize-string.java
+++ 767-reorganize-string/767-reorganize-string.java
@@ -1,28 +1,39 @@
 class Solution {
     public String reorganizeString(String s) {
         HashMap<Character, Integer> map = new HashMap<>();
+        int max = 0;
+        char maxChar = '.';
         
         for (int i = 0; i < s.length(); i++) {
             map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
             if (map.get(s.charAt(i)) > (s.length() + 1)/2) {
                 return "";
             }
-        }
-        
-        PriorityQueue<Map.Entry<Character, Integer>> maxHeap = new PriorityQueue<>((e1, e2) -> e2.getValue() - e1.getValue());
-        
-        for (Map.Entry<Character, Integer> e : map.entrySet()) {
-            maxHeap.add(e);
+            if (map.get(s.charAt(i)) > max) {
+                max = map.get(s.charAt(i));
+                maxChar = s.charAt(i);
+            }
         }
         
         char[] out = new char[s.length()];
         int idx = 0;
-        Map.Entry<Character, Integer> curr;
         
-        while (!maxHeap.isEmpty()) {
-            curr = maxHeap.poll();
-            for (int i = 0; i < curr.getValue(); i++) {
-                out[idx] = curr.getKey();
+        for (int i = 0; i < map.get(maxChar); i++) {
+            out[idx] = maxChar;
+            idx += 2;
+
+            if (idx >= s.length()) {
+                idx = 1;
+            }
+        }
+        
+        map.remove(maxChar);
+               
+
+        
+        for (Map.Entry<Character, Integer> e : map.entrySet()) {
+            for (int i = 0; i < e.getValue(); i++) {
+                out[idx] = e.getKey();
                 idx += 2;
                 
                 if (idx >= s.length()) {
@@ -30,7 +41,7 @@ class Solution {
                 }
             }
         }
-        
+
         return new String(out);
         
     }
