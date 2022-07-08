49efe1183db226357590ee75f62a6fa231252305
NishantTharani <33641044+NishantTharani@users.noreply.github.com>
2022-07-08 15:07:20 -0700

    Time: 65 ms (48.39%), Space: 56.9 MB (46.08%) - LeetHub

--- 632-smallest-range-covering-elements-from-k-lists/632-smallest-range-covering-elements-from-k-lists.java
+++ 632-smallest-range-covering-elements-from-k-lists/632-smallest-range-covering-elements-from-k-lists.java
@@ -3,26 +3,29 @@ class Solution {
         int[] out = new int[]{0, Integer.MAX_VALUE};
         int k = nums.size();
         int max = Integer.MIN_VALUE;
+        int[] idxs = new int[k];
         
-        PriorityQueue<Pair<Integer,Integer>> minHeap = new PriorityQueue<>((p1, p2) -> Integer.compare(nums.get(p1.getKey()).get(p1.getValue()), nums.get(p2.getKey()).get(p2.getValue())));
+        PriorityQueue<Pair<Integer,Integer>> minHeap = new PriorityQueue<>((p1, p2) -> Integer.compare(p1.getValue(), p2.getValue()));
                 
         for (int i = 0; i < k; i++) {
-            minHeap.add(new Pair<Integer,Integer>(i,0));
+            minHeap.add(new Pair<Integer,Integer>(i, nums.get(i).get(0)));
             max = Math.max(max, nums.get(i).get(0));
+            idxs[i] = 1;
         }
         
         while (minHeap.size() == k) {
             Pair<Integer,Integer> minPair = minHeap.poll();
-            int min = nums.get(minPair.getKey()).get(minPair.getValue());
+            int min = minPair.getValue();
             
             if (max - min < out[1] - out[0]) {
                 out[1] = max;
                 out[0] = min;
             }
             
-            if (minPair.getValue() < nums.get(minPair.getKey()).size() - 1) {
-                minHeap.add(new Pair<Integer,Integer>(minPair.getKey(), minPair.getValue()+1));
-                max = Math.max(max, nums.get(minPair.getKey()).get(minPair.getValue()+1));
+            if (idxs[minPair.getKey()] < nums.get(minPair.getKey()).size()) {
+                minHeap.add(new Pair<Integer,Integer>(minPair.getKey(), nums.get(minPair.getKey()).get(idxs[minPair.getKey()])));
+                max = Math.max(max, nums.get(minPair.getKey()).get(idxs[minPair.getKey()]));
+                idxs[minPair.getKey()]++;
             }
         }
         
