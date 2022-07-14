91aa53077c0a20e16d43221de003ea3377fdbbb4
NishantTharani <33641044+NishantTharani@users.noreply.github.com>
2022-07-14 15:03:35 -0700

    Time: 44 ms (54.38%), Space: 56.7 MB (14.84%) - LeetHub

--- 494-target-sum/494-target-sum.java
+++ 494-target-sum/494-target-sum.java
@@ -1,26 +1,25 @@
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
         int k = nums.length;
-        int sum = 1000;
         
-        int[][] val = new int[2*sum + 1][k+1];
+        int[][] val = new int[2001][k+1];
         
-        for (int i = 0; i <= 2*sum; i++)
+        for (int i = 0; i <= 2000; i++)
             val[i][0] = 0;
-        val[sum][0] = 1;
+        val[1000][0] = 1;
         
         for (int j = 1; j <= k; j++) {
-            for (int i = 0; i <= 2*sum; i++) {
+            for (int i = 0; i <= 2000; i++) {
                 int num = nums[j-1];
                 int v = 0;
                 if (i - num >= 0)
                     v += val[i - num][j-1];
-                if (i + num <= 2*sum)
+                if (i + num <= 2000)
                     v += val[i + num][j-1];
                 val[i][j] = v;
             }
         }
         
-        return val[target+sum][k];
+        return val[target+1000][k];
     }
 }
