48cfb1de52cba826c776a4663859239bfbed777c
NishantTharani <33641044+NishantTharani@users.noreply.github.com>
2022-07-22 16:45:49 -0700

    Time: 48 ms (22.36%), Space: 73.1 MB (5.25%) - LeetHub

--- 720-longest-word-in-dictionary/720-longest-word-in-dictionary.java
+++ 720-longest-word-in-dictionary/720-longest-word-in-dictionary.java
@@ -1,26 +1,65 @@
 class Solution {
     public String longestWord(String[] words) {
-        Arrays.sort(words, new Comparator<String>(){
-            public int compare(String one, String two) {
-                if (one.length() != two.length()) {
-                    return one.length() - two.length();
-                } else {
-                    return one.compareTo(two);
-                }
-            }
-        });
-        Set<String> possible = new HashSet<>();
-        String out = "";
-        possible.add("");
+        Trie trie = new Trie(words);
+        for (int i = 0; i < words.length; i++)
+            trie.insert(words[i], i);
+        return trie.answerSearch();
+    }
+    
+    private class Node {
+        char c;
+        int index;
+        HashMap<Character, Node> children;
         
-        for (String s : words) {
-            if (possible.contains(s.substring(0, s.length() - 1))) {
-                possible.add(s);
-                if (s.length() > out.length())
-                    out = s;
-            }
+        public Node(char c) {
+            this.c = c;
+            this.children = new HashMap<>();
+            this.index = -1;
+        }
+    }
+    
+    private class Trie {
+        Node root;
+        String[] words;
+        
+        public Trie(String[] words) {
+            this.root = new Node('0');
+            this.words = words;
         }
         
-        return out;
+        public void insert(String s, int index) {
+            Node curr = root;
+            for (char c : s.toCharArray()) {
+                curr.children.putIfAbsent(c, new Node(c));
+                curr = curr.children.get(c);
+            }
+            curr.index = index;
+        }
+        
+        public String answerSearch() {
+            String out = "";
+            String word;
+            Node curr;
+            Node next;
+            Stack<Node> stack = new Stack<>();
+            stack.push(root);
+            
+            while (!stack.isEmpty()) {
+                curr = stack.pop();
+                for (Character key : curr.children.keySet()) {
+                    next = curr.children.get(key);
+                    if (next.index >= 0)
+                        stack.push(next);
+                }
+                
+                if (curr.index >= 0) {
+                    word = this.words[curr.index];
+                    if (word.length() > out.length() || (word.length() == out.length() && word.compareTo(out) < 0))
+                        out = word;
+                }
+            }
+            
+            return out;
+        }
     }
 }
