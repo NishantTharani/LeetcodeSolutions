124a81e2671c5b637ad5bbac2a81dbfafd3ad29a
NishantTharani <33641044+NishantTharani@users.noreply.github.com>
2022-07-26 11:37:34 -0700

    Time: 64 ms (49.55%), Space: 56.6 MB (63.50%) - LeetHub

--- 632-smallest-range-covering-elements-from-k-lists/632-smallest-range-covering-elements-from-k-lists.java
+++ 632-smallest-range-covering-elements-from-k-lists/632-smallest-range-covering-elements-from-k-lists.java
@@ -7,23 +7,22 @@ class Solution {
         int k = nums.size();
         int[] idxs = new int[k];
         PriorityQueue<Pair<Integer,Integer>> minHeap = new PriorityQueue<>((p1, p2) -> Integer.compare(p1.getKey(), p2.getKey()));
-        PriorityQueue<Pair<Integer,Integer>> maxHeap = new PriorityQueue<>((p1, p2) -> Integer.compare(p2.getKey(), p1.getKey()));
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
         
         for (int i = 0; i < k; i++) {
             minHeap.add(new Pair<Integer,Integer>(nums.get(i).get(0), i));
-            maxHeap.add(new Pair<Integer,Integer>(nums.get(i).get(0), i));
+            maxHeap.add(nums.get(i).get(0));
             idxs[i] = 1;
         }
         
         int[] out = new int[2];
         out[0] = minHeap.peek().getKey();
-        out[1] = maxHeap.peek().getKey();
+        out[1] = maxHeap.peek();
         
         while (minHeap.size() == k) {
             Pair<Integer,Integer> minPair = minHeap.poll();
-            Pair<Integer,Integer> maxPair = maxHeap.peek();
             int min = minPair.getKey();
-            int max = maxPair.getKey();
+            int max = maxHeap.peek();
             
             if (max - min < out[1] - out[0]) {
                 out[0] = min;
@@ -33,7 +32,7 @@ class Solution {
             int j = minPair.getValue();
             if (idxs[j] < nums.get(j).size()) {
                 minHeap.add(new Pair<Integer,Integer>(nums.get(j).get(idxs[j]), j));
-                maxHeap.add(new Pair<Integer,Integer>(nums.get(j).get(idxs[j]), j));
+                maxHeap.add(nums.get(j).get(idxs[j]));
                 idxs[j]++;
             }
         }
