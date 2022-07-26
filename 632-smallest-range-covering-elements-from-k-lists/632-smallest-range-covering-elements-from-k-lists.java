525e530d6be48cf57153e6390f7a556dfb099a6c
NishantTharani <33641044+NishantTharani@users.noreply.github.com>
2022-07-26 11:36:32 -0700

    Time: 70 ms (40.96%), Space: 54.7 MB (80.94%) - LeetHub

--- 632-smallest-range-covering-elements-from-k-lists/632-smallest-range-covering-elements-from-k-lists.java
+++ 632-smallest-range-covering-elements-from-k-lists/632-smallest-range-covering-elements-from-k-lists.java
@@ -1,31 +1,40 @@
 class Solution {
     public int[] smallestRange(List<List<Integer>> nums) {
-        int[] out = new int[]{0, Integer.MAX_VALUE};
-        int k = nums.size();
-        int max = Integer.MIN_VALUE;
-        int[] idxs = new int[k];
+        // Put the head of each list into a minHeap and a maxHeap
+        // At each step, check the range between the top of each heap and update the smallest range if necessary
+        // Then poll the top of each heap and replace it with the next element in the list that element came from
         
-        PriorityQueue<Pair<Integer,Integer>> minHeap = new PriorityQueue<>((p1, p2) -> Integer.compare(p1.getValue(), p2.getValue()));
-                
+        int k = nums.size();
+        int[] idxs = new int[k];
+        PriorityQueue<Pair<Integer,Integer>> minHeap = new PriorityQueue<>((p1, p2) -> Integer.compare(p1.getKey(), p2.getKey()));
+        PriorityQueue<Pair<Integer,Integer>> maxHeap = new PriorityQueue<>((p1, p2) -> Integer.compare(p2.getKey(), p1.getKey()));
+        
         for (int i = 0; i < k; i++) {
-            minHeap.add(new Pair<Integer,Integer>(i, nums.get(i).get(0)));
-            max = Math.max(max, nums.get(i).get(0));
+            minHeap.add(new Pair<Integer,Integer>(nums.get(i).get(0), i));
+            maxHeap.add(new Pair<Integer,Integer>(nums.get(i).get(0), i));
             idxs[i] = 1;
         }
         
+        int[] out = new int[2];
+        out[0] = minHeap.peek().getKey();
+        out[1] = maxHeap.peek().getKey();
+        
         while (minHeap.size() == k) {
             Pair<Integer,Integer> minPair = minHeap.poll();
-            int min = minPair.getValue();
+            Pair<Integer,Integer> maxPair = maxHeap.peek();
+            int min = minPair.getKey();
+            int max = maxPair.getKey();
             
             if (max - min < out[1] - out[0]) {
-                out[1] = max;
                 out[0] = min;
+                out[1] = max;
             }
             
-            if (idxs[minPair.getKey()] < nums.get(minPair.getKey()).size()) {
-                minHeap.add(new Pair<Integer,Integer>(minPair.getKey(), nums.get(minPair.getKey()).get(idxs[minPair.getKey()])));
-                max = Math.max(max, nums.get(minPair.getKey()).get(idxs[minPair.getKey()]));
-                idxs[minPair.getKey()]++;
+            int j = minPair.getValue();
+            if (idxs[j] < nums.get(j).size()) {
+                minHeap.add(new Pair<Integer,Integer>(nums.get(j).get(idxs[j]), j));
+                maxHeap.add(new Pair<Integer,Integer>(nums.get(j).get(idxs[j]), j));
+                idxs[j]++;
             }
         }
         
