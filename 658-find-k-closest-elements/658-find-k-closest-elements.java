2c8acd4d304862ca3d99e9907bcd798ca987b5a5
NishantTharani <33641044+NishantTharani@users.noreply.github.com>
2022-07-26 18:18:55 -0700

    Time: 7 ms (65.27%), Space: 62.5 MB (50.49%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.java
+++ 658-find-k-closest-elements/658-find-k-closest-elements.java
@@ -1,54 +1,44 @@
 class Solution {
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
-        int gap;
+        // Binary search to find the location of the closest number to x
+        
         int left = 0;
         int right = arr.length - 1;
-        int mid;
         
-        // First run binary search
-        while (left <= right) {
-            mid = left + (right - left)/2;
+        while (left != right) {
+            int mid = (left + right + 1)/2;
             
-            if (arr[mid] == x) {
+            if (x == arr[mid]) {
                 left = mid;
                 right = mid;
-                break;
             } else if (x < arr[mid]) {
                 right = mid - 1;
             } else {
-                left = mid + 1;
+                left = mid;
             }
         }
         
-        // Now set left and right to be the bounds of the subarray that we will return
-        if (right < 0) {
-            right = left;
-        } else if (left >= arr.length) {
-            left = right;
-        } else if (Math.abs(arr[right] - x) <= Math.abs(arr[left] - x)) {
-            left = right;
-        } else {
-            right = left;
+        if (left < arr.length - 1 && Math.abs(arr[left] - x) > Math.abs(arr[left+1] - x)) {
+            left++;
+            right++;
         }
         
-        // Now expand the subarray until we've found enough elements
-        while ((right - left + 1) < k) {
+        // Now arr[left] is the closest element to x
+        while (right - left + 1 < k) {
             if (left == 0) {
                 right++;
             } else if (right == arr.length - 1) {
                 left--;
-            } else if (Math.abs(arr[left-1] - x) <= Math.abs(arr[right+1] - x)) {
+            } else if (Math.abs(x - arr[left-1]) <= Math.abs(x - arr[right+1])) {
                 left--;
             } else {
                 right++;
             }
         }
         
-        // Copy the subarray
         List<Integer> out = new ArrayList<>();
-        for (int i = left; i <= right; i++) {
+        for (int i = left; i <= right; i++)
             out.add(arr[i]);
-        }
         
         return out;
     }
