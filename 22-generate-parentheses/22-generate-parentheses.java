1456064c294f3f33dc9bbd87c58d93af39f33b3b
NishantTharani <33641044+NishantTharani@users.noreply.github.com>
2022-07-27 13:31:20 -0700

    Time: 4 ms (26.63%), Space: 43.8 MB (49.35%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.java
+++ 22-generate-parentheses/22-generate-parentheses.java
@@ -1,23 +1,31 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
-        Queue<String> queue = new LinkedList<>();
-        queue.add("()");
+        Queue<StringBuilder> queue = new LinkedList<>();
+        queue.add(new StringBuilder("()"));
         
         for (int i = 1; i < n; i++) {
             int k = queue.size();
             
             for (int j = 0; j < k; j++) {
-                String s = queue.poll();
-                queue.add("()" + s);
+                StringBuilder sb = queue.poll();
+                StringBuilder next = new StringBuilder(sb.toString());
+                next.insert(0, "()");
+                queue.add(next);
                 int a = 0;
-                while (s.charAt(a) != ')') {
-                    queue.add(s.substring(0, a+1) + "()" + s.substring(a+1, s.length()));
+                while (sb.charAt(a) != ')') {
+                    next = new StringBuilder(sb.toString());
+                    next.insert(a+1, "()");
+                    queue.add(next);
                     a++;
                 }
             }
         }
         
-        return new ArrayList<String>(queue);
+        List<String> out = new ArrayList<String>();
+        while (!queue.isEmpty()) {
+            out.add(queue.poll().toString());
+        }
+        return out;
     }
 }
 
