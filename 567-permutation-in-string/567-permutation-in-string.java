adfd4b5f5571bc0ebe00217f547d88da9486693e
NishantTharani <33641044+NishantTharani@users.noreply.github.com>
2022-08-03 17:37:37 -0700

    Time: 10 ms (60.02%), Space: 43.3 MB (60.69%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.java
+++ 567-permutation-in-string/567-permutation-in-string.java
@@ -1,32 +1,32 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        // Create the char array of s1
-        int[] s1chars = new int[26];
+        if (s2.length() < s1.length())
+            return false;
+        
+        int[] freq = new int[26];
+        int[] match = new int[26];
+        
         for (int i = 0; i < s1.length(); i++) {
-            s1chars[s1.charAt(i) - 'a']++;
+            freq[s1.charAt(i)-97]++;
         }
         
-        // Slide window
-        int[] s2chars = new int[26];
-        int count = 0;
-        char s2char;
         int start = 0;
-        for (int i = 0; i < s2.length(); i++) {
-            s2char = s2.charAt(i);
-            s2chars[s2char - 'a']++;
-            count++;
+        
+        for (int end = 0; end < s2.length(); end++) {
+            char c = s2.charAt(end);
+            match[c-97]++;
             
-            while (s2chars[s2char - 'a'] > s1chars[s2char - 'a']) {
-                s2chars[s2.charAt(start) - 'a']--;
-                count--;
+            while (match[c-97] > freq[c-97]) {
+                match[s2.charAt(start)-97]--;
                 start++;
             }
             
-            if (count == s1.length()) {
+            if (end - start + 1 == s1.length())
                 return true;
-            }
         }
         
         return false;
+        
+        
     }
 }
