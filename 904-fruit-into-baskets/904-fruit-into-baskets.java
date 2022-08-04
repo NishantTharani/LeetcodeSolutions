38da0da98c519cc41c1d07d778813b1b4bd8f305
NishantTharani <33641044+NishantTharani@users.noreply.github.com>
2022-08-03 20:11:23 -0700

    Time: 103 ms (33.34%), Space: 115.6 MB (11.13%) - LeetHub

--- 904-fruit-into-baskets/904-fruit-into-baskets.java
+++ 904-fruit-into-baskets/904-fruit-into-baskets.java
@@ -1,22 +1,28 @@
 class Solution {
     public int totalFruit(int[] fruits) {
-        int max = 0;
-        Map<Integer, Integer> map = new HashMap<>();
+        // Sliding windows
+        
+        int out = 0;
+        Map<Integer,Integer> map = new HashMap<>();
+        int picked = 0;
         int start = 0;
         
         for (int end = 0; end < fruits.length; end++) {
             map.put(fruits[end], map.getOrDefault(fruits[end], 0) + 1);
             
-            while (map.size() > 2) {
+            if (map.get(fruits[end]) == 1)
+                picked++;
+            
+            while (picked > 2) {
                 map.put(fruits[start], map.get(fruits[start]) - 1);
                 if (map.get(fruits[start]) == 0)
-                    map.remove(fruits[start]);
+                    picked--;
                 start++;
             }
             
-            max = Math.max(max, end + 1 - start);
+            out = Math.max(out, end + 1 - start);
         }
         
-        return max;
+        return out;
     }
 }
