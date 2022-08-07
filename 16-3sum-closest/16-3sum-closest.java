81e7fa60b0237e2efe79a970701388e2c0bcb1c9
NishantTharani <33641044+NishantTharani@users.noreply.github.com>
2022-08-06 17:49:30 -0700

    Time: 96 ms (68.71%), Space: 48.8 MB (63.76%) - LeetHub

--- 16-3sum-closest/16-3sum-closest.java
+++ 16-3sum-closest/16-3sum-closest.java
@@ -4,19 +4,29 @@ class Solution {
         int closest = nums[0] + nums[1] + nums[2];
         
         for (int i = 0; i < nums.length - 2; i++) {
+            
+            if (i > 0 && nums[i] == nums[i-1])
+                continue;
+            
             int j = i+1;
             int k = nums.length - 1;
             
             while (j < k) {
-                int sum = nums[i] + nums[j] + nums[k];
-                if (sum == target)
-                    return sum;
-                if (Math.abs(target - sum) < Math.abs(closest - target))
-                    closest = sum;
-                if (sum < target)
+                if (j > i+1 && nums[j] == nums[j-1])
                     j++;
-                else
+                else if (k < nums.length - 1 && nums[k] == nums[k+1])
                     k--;
+                else {
+                    int sum = nums[i] + nums[j] + nums[k];
+                    if (sum == target)
+                        return sum;
+                    if (Math.abs(target - sum) < Math.abs(closest - target))
+                        closest = sum;
+                    if (sum < target)
+                        j++;
+                    else
+                        k--;
+                }
             }
         }
         
