cf56e5659ac0a193c00d9b0abdeaaa4513b1db07
NishantTharani <33641044+NishantTharani@users.noreply.github.com>
2022-08-08 17:29:42 -0700

    Time: 63 ms (5.07%), Space: 214.2 MB (5.22%) - LeetHub

---
+++ 131-palindrome-partitioning/131-palindrome-partitioning.java
@@ -0,0 +1,57 @@
+class Solution {
+    public List<List<String>> partition(String s) {
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        int n = s.length();
+        
+        for (int i = 0; i < n; i++)
+            map.put(i, new ArrayList<>());
+        
+        for (int i = 0; i < n; i++) {
+            int left = i;
+            int right = i;
+            
+            while (left >= 0 && right < n && s.charAt(left) == s.charAt(right)) {
+                map.get(left).add(right);
+                left--;
+                right++;
+            }
+            
+            left = i;
+            right = i+1;
+            
+            while (left >= 0 && right < n && s.charAt(left) == s.charAt(right)) {
+                map.get(left).add(right);
+                left--;
+                right++;
+            }
+        }
+        
+        Queue<List<Integer>> queue = new LinkedList<>();
+        List<Integer> start = new ArrayList<>();
+        start.add(0);
+        queue.add(start);
+        List<List<String>> out = new ArrayList<>();
+        
+        while (!queue.isEmpty()) {
+            List<Integer> curr = queue.poll();
+            int idx = curr.get(curr.size() - 1);
+            
+            if (idx == n) {
+                // Convert into a partition and store in out
+                List<String> partition = new ArrayList<>();
+                for (int i = 0; i < curr.size() - 1; i++) {
+                    partition.add(s.substring(curr.get(i), curr.get(i+1)));
+                }
+                out.add(partition);
+            } else {
+                for (int end : map.get(idx)) {
+                    List<Integer> next = new ArrayList<>(curr);
+                    next.add(end + 1);
+                    queue.add(next);
+                }
+            }
+        }
+        
+        return out;
+    }
+}
