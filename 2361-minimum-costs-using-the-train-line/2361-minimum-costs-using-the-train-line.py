7e5f6019ebfa674230d9ec2bcac6119658f3978f
NishantTharani <33641044+NishantTharani@users.noreply.github.com>
2022-09-01 11:34:19 -0700

    Time: 3348 ms (8.79%), Space: 42.1 MB (36.40%) - LeetHub

--- 2361-minimum-costs-using-the-train-line/2361-minimum-costs-using-the-train-line.py
+++ 2361-minimum-costs-using-the-train-line/2361-minimum-costs-using-the-train-line.py
@@ -2,15 +2,14 @@ class Solution:
     def minimumCosts(self, regular: List[int], express: List[int], expressCost: int) -> List[int]:
         regular_val = []
         express_val = []
+        out = []
         regular_val.append(regular[0])
         express_val.append(expressCost + express[0])
+        out.append(min(regular_val[0], express_val[0]))
         
         for i in range(1, len(regular)):
             regular_val.append(min(regular_val[i-1] + regular[i], express_val[i-1] + regular[i]))
             express_val.append(min(regular_val[i-1] + expressCost + express[i], express_val[i-1] + express[i]))
-        
-        out = []
-        for r, e in zip(regular_val, express_val):
-            out.append(min(r, e))
+            out.append(min(regular_val[i], express_val[i]))
         
         return out
