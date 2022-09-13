171fcdbf1d7d0653d3d1d0b86f1f9980929b3659
NishantTharani <33641044+NishantTharani@users.noreply.github.com>
2022-09-13 16:51:30 -0700

    Time: 208 ms (14.42%), Space: 13.8 MB (93.25%) - LeetHub

---
+++ 40-combination-sum-ii/40-combination-sum-ii.py
@@ -0,0 +1,24 @@
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        candidates.sort()
+        out = []
+        self.rec(candidates, 0, target, [], out)
+        return out
+    
+    
+    def rec(self, candidates, idx, target, curr, out):
+        if target == 0:
+            out.append(list(curr))
+            return
+        
+        if idx >= len(candidates):
+            return
+        
+        next_idx = idx
+        while next_idx < len(candidates) and candidates[next_idx] == candidates[idx]:
+            next_idx += 1
+        
+        self.rec(candidates, next_idx, target, curr, out)
+        
+        if target - candidates[idx] >= 0:
+            self.rec(candidates, idx+1, target - candidates[idx], list(curr) + [candidates[idx]], out)
