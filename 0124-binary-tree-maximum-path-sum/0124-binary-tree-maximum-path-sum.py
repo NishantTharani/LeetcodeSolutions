ed82bf92e9b4fc8d3c6b40f494077ab58345cbc9
NishantTharani <33641044+NishantTharani@users.noreply.github.com>
2022-10-13 12:34:45 -0700

    Time: 189 ms (33.90%), Space: 21.3 MB (94.49%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.py
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.py
@@ -6,26 +6,26 @@
 #         self.right = right
 class Solution:
     def maxPathSum(self, root: Optional[TreeNode]) -> int:
-        max_sum = float('-inf')
+        # max_sum = float('-inf')
         
-        def rec(root):
-            nonlocal max_sum
+        def rec(root, curr_sum):
+            # nonlocal max_sum
             
             # Base case: node is None
             if root is None:
-                return 0
+                return (0, curr_sum)
             
             # Recurse left and right to get the max downward path starting from that node
-            go_left = rec(root.left)
-            go_right = rec(root.right)
+            go_left, curr_sum = rec(root.left, curr_sum)
+            go_right, curr_sum = rec(root.right, curr_sum)
             
             # Get max path sum going through root and update max_sum
             sum_through_root = root.val + go_left + go_right
-            max_sum = max(sum_through_root, max_sum)
+            next_curr_sum = max(sum_through_root, curr_sum)
             
             # Return max downward path starting from this node
-            return max(max(go_left, go_right) + root.val, 0)
+            return (max(max(go_left, go_right) + root.val, 0), next_curr_sum)
         
-        rec(root)
-        return max_sum
+        _, out = rec(root, float('-inf'))
+        return out
 
