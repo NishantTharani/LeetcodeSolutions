f3f1a9d0c2d4e8bd3e92bb81b1e90b5596e4b74b
Nishant Tharani <33641044+NishantTharani@users.noreply.github.com>
2023-10-09 14:32:08 -0700

    Time: 35 ms (87.33%), Space: 16.2 MB (64.25%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.py
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -9,12 +9,26 @@ class Solution:
             # Add left outer number
             row.append(1)
 
-            for j in range(i-1):
-                # Add interior numbers
+            # We want to fill in the row through index i//2
+            for j in range(i//2):
+                # j = 0 means we're adding a number at index 1
                 row.append(above_row[j] + above_row[j+1])
 
-            # Add right outer number
-            row.append(1)
+            """
+            Now we've filled in row[0] through row[i//2]
+            If i is odd:
+                We want to copy row[0:i//2] to row[i//2+1:i]
+            If i is even:
+                We want to copy row[0:i//2-1] to row[i//2+1:i]
+            """
+
+            if i % 2 == 1:
+                for j in range(i//2, -1, -1):
+                    row.append(row[j])
+            else:
+                for j in range(i//2 - 1, -1, -1):
+                    row.append(row[j])
+
 
             out.append(row)
         
