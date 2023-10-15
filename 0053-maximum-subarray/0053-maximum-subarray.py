8bc9123bcb8704c4e053d898a99f8c2bfaac07f7
Nishant Tharani <33641044+NishantTharani@users.noreply.github.com>
2023-10-14 17:55:01 -0700

    Time: 621 ms (30.75%), Space: 30.5 MB (28.66%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -1,23 +1,10 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         out = -float('inf')
-        start = 0
         current_sum = 0
-
-        for end in range(len(nums)):
-            # Add nums[end] to the sliding window
-            current_sum += nums[end]
-
-            # Move up the start of the sliding window
-            while start < end and nums[start] < 0:
-                current_sum -= nums[start]
-                start += 1
-            
-            # If nums[end] alone would be better, move up start some more
-            if nums[end] > current_sum:
-                start = end
-                current_sum = nums[end]
-            
+        
+        for num in nums:
+            current_sum = max(num, current_sum + num)
             out = max(out, current_sum)
         
         return out
